86189f8c241f8544737516f89d2b4103412ed53e
jianchuxin <903243301@qq.com>
2023-06-12 21:09:40 +0800

    封装axios接口

---
+++ src/utils/axios.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const request = axios.create({
+  baseURL: import.meta.env.VUE_APP_BASE_API_URL,
+  timeout: 5000,
+});
+
+// axios请求拦截器
+request.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (e) => {
+    return Promise.reject(e);
+  }
+);
+
+// axios响应拦截器
+request.interceptors.response.use(
+  (res) => {
+    return res.data;
+  },
+  (e) => {
+    return Promise.reject(e);
+  }
+);
+
+export default request;
